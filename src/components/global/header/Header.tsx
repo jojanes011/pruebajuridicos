10692d123c5112b8dfabbe1bf085dc4d882b3c49
Joan Oviedo <jsteran@stefanini.com>
2024-11-21 14:40:06 -0500

    Refactor Header component for improved mobile responsiveness and styling

--- src/components/global/header/Header.tsx
+++ src/components/global/header/Header.tsx
@@ -28,66 +28,122 @@ export default function Header({ showBorder = true, hasBackground = false }) {
 
   const toggleMobileMenu = () => {
     setMobileMenuOpen(!mobileMenuOpen);
-    if (mobileMenuOpen) setOpenCollapse(false); // Cerrar collapse al cerrar el menú móvil
+    // Asegura que el colapso se cierre al cerrar el menú móvil
+    if (mobileMenuOpen) {
+      setOpenCollapse(false);
+    }
   };
 
   useEffect(() => {
     const handleScroll = () => {
-      setIsScrolled(window.scrollY > 50); // Cambia el estado al hacer scroll
+      // Cambia el estado cuando el scroll pasa 100px, por ejemplo
+      setIsScrolled(window.scrollY > 100);
     };
+
+    // Agrega el evento de escucha de scroll
     window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
+
+    // Limpia el evento al desmontar el componente
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
 
   return (
     <AppBar
-      position="fixed"
+      position="sticky"
+      color="transparent"
       elevation={0}
       sx={{
         backgroundColor:
           isScrolled || hasBackground ? "#511011" : "transparent",
-        transition: "background-color 0.3s ease, box-shadow 0.3s ease",
-        boxShadow:
-          isScrolled && showBorder ? "0 2px 4px rgba(0, 0, 0, 0.1)" : "",
-        zIndex: 20, // Asegura que el header esté sobre el video
+        transition: "background-color 0.3s",
+        boxShadow: showBorder ? "0 0 0 0.5px white" : "",
       }}
+      className="text-white"
     >
-      <Toolbar className="w-full sm:w-[1200px] mx-auto flex justify-between items-center py-4">
-        {/* Logo */}
-        <Link href="/" aria-label="Inicio">
-          <div className="flex items-center space-x-4">
+      <Toolbar className="w-full sm:w-[1200px] mx-auto" disableGutters>
+        <div className="sm:flex flex-1 py-4 hidden">
+          <Link
+            href="/"
+            aria-label="Inicio"
+            className="relative flex flex-row items-center space-x-8"
+          >
             <Image
               src="/images/logo-white.png"
               alt="Logotipo de Jurídicos y Asociados"
-              width={48}
-              height={48}
+              width={64}
+              height={64}
             />
-            <h1 className="text-white text-xl hidden sm:block">
-              JURÍDICOS Y ASOCIADOS
-            </h1>
-          </div>
-        </Link>
+            <h1 className=" text-xl">JURÍDICOS Y ASOCIADOS</h1>
+          </Link>
+        </div>
+
+        {/* Menú móvil */}
+        <nav className="flex sm:hidden flex-row items-center justify-between w-full py-2 pl-4 pr-2">
+          <Link
+            href="/"
+            aria-label="Inicio"
+            className="relative flex flex-row items-center space-x-8 h-12 w-12 sm:w-16 sm:h-16"
+          >
+            <Image
+              src="/images/logo-white.png"
+              alt="Logotipo de Jurídicos y Asociados"
+              layout="fill"
+              style={{
+                objectFit: "contain",
+              }}
+            />
+          </Link>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={toggleMobileMenu}
+          >
+            <Image
+              src="/icons/menu.svg"
+              alt="Icono Menú"
+              width={32}
+              height={32}
+            />
+          </IconButton>
+        </nav>
 
         {/* Menú principal en escritorio */}
-        <nav className="hidden sm:flex space-x-8 items-center">
+        <div className="hidden sm:flex flex-row items-center flex-1 space-x-8 py-4 justify-end">
           <Link href="/">
-            <MenuItem className="text-white">Inicio</MenuItem>
+            <MenuItem>Inicio</MenuItem>
           </Link>
           <button
-            aria-controls="especialidades-menu"
+            aria-controls="asesoria-menu"
             aria-haspopup="true"
+            className="w-full whitespace-nowrap overflow-hidden text-ellipsis"
             onClick={handleClick}
-            className="text-white"
           >
             Especialidades
           </button>
           <Menu
-            id="especialidades-menu"
+            id="asesoria-menu"
             anchorEl={anchorEl}
+            keepMounted
             open={open}
             onClose={handleClose}
-            anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
-            transformOrigin={{ vertical: "top", horizontal: "left" }}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "left",
+            }}
+            slotProps={{
+              paper: {
+                style: {
+                  width: "180px",
+                },
+              },
+            }}
           >
             <Link href="/servicios/derecho-civil">
               <MenuItem>Derecho Civil</MenuItem>
@@ -98,33 +154,27 @@ export default function Header({ showBorder = true, hasBackground = false }) {
             <Link href="/servicios/derecho-penal">
               <MenuItem>Derecho Penal</MenuItem>
             </Link>
+            <Link href="/servicios/derecho-laboral">
+              <MenuItem>Derecho Laboral</MenuItem>
+            </Link>
           </Menu>
           <Link href="/blog">
-            <MenuItem className="text-white">Artículos</MenuItem>
+            <MenuItem>Artículos</MenuItem>
           </Link>
           <Link href="/contacto">
-            <MenuItem className="text-white">Contacto</MenuItem>
+            <MenuItem>Contacto</MenuItem>
           </Link>
-          <a
-            href={whatsappLink}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="bg-secondary text-white rounded py-2 px-6"
-          >
-            Contáctanos
-          </a>
-        </nav>
-
-        {/* Menú móvil */}
-        <IconButton
-          edge="start"
-          color="inherit"
-          aria-label="open drawer"
-          onClick={toggleMobileMenu}
-          className="sm:hidden"
-        >
-          <Image src="/icons/menu.svg" alt="Menú" width={32} height={32} />
-        </IconButton>
+          <div className="flex flex-row justify-center">
+            <a
+              href={whatsappLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="bg-secondary text-white rounded py-3 px-8"
+            >
+              Contáctanos
+            </a>
+          </div>
+        </div>
       </Toolbar>
 
       {/* Drawer del menú móvil */}
@@ -132,55 +182,89 @@ export default function Header({ showBorder = true, hasBackground = false }) {
         anchor="left"
         open={mobileMenuOpen}
         onClose={toggleMobileMenu}
-        transitionDuration={300}
+        transitionDuration={300} // Ajusta la duración de la animación
       >
-        {/* Contenido del Drawer */}
-        <div className="w-screen bg-primary h-screen text-white">
-          <div className="flex justify-between p-4">
-            <Link href="/">
+        <div className="w-screen bg-primary h-screen" role="presentation">
+          <div className="flex flex-row justify-between p-4">
+            <Link
+              href="/"
+              aria-label="Inicio"
+              className="flex flex-row items-center space-x-8"
+            >
               <Image
                 src="/images/logo-white.png"
-                alt="Logo"
+                alt="Logotipo de Jurídicos y Asociados"
                 width={64}
                 height={64}
               />
             </Link>
-            <IconButton onClick={toggleMobileMenu}>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="close drawer"
+              onClick={toggleMobileMenu}
+            >
               <Image
                 src="/icons/close.svg"
-                alt="Cerrar"
+                alt="Icono Menú"
                 width={32}
                 height={32}
               />
             </IconButton>
           </div>
-          <nav>
-            <MenuItem onClick={toggleMobileMenu}>
-              <Link href="/">Inicio</Link>
+
+          {/* Enlaces del Drawer */}
+          <MenuItem disableGutters onClick={toggleMobileMenu}>
+            <Link href="/" className="font-bold text-white pl-4">
+              Inicio
+            </Link>
+          </MenuItem>
+          <button
+            aria-controls="especialidades-menu"
+            aria-haspopup="true"
+            className="w-full text-left text-white"
+            onClick={() => setOpenCollapse(!openCollapse)}
+          >
+            <MenuItem disableGutters>
+              <div className="font-bold text-white pl-4">Especialidades</div>
             </MenuItem>
-            <MenuItem onClick={() => setOpenCollapse(!openCollapse)}>
-              Especialidades
+          </button>
+          <Collapse in={openCollapse} className="text-white pl-6">
+            <MenuItem>
+              <Link href="/servicios/derecho-civil">Derecho Civil</Link>
             </MenuItem>
-            <Collapse in={openCollapse}>
-              <MenuItem>
-                <Link href="/servicios/derecho-civil">Derecho Civil</Link>
-              </MenuItem>
-              <MenuItem>
-                <Link href="/servicios/derecho-de-familia">
-                  Derecho de Familia
-                </Link>
-              </MenuItem>
-              <MenuItem>
-                <Link href="/servicios/derecho-penal">Derecho Penal</Link>
-              </MenuItem>
-            </Collapse>
-            <MenuItem onClick={toggleMobileMenu}>
-              <Link href="/blog">Artículos</Link>
+            <MenuItem>
+              <Link href="/servicios/derecho-de-familia">
+                Derecho de Familia
+              </Link>
             </MenuItem>
-            <MenuItem onClick={toggleMobileMenu}>
-              <Link href="/contacto">Contacto</Link>
+            <MenuItem>
+              <Link href="/servicios/derecho-penal">Derecho Penal</Link>
             </MenuItem>
-          </nav>
+            <MenuItem>
+              <Link href="/servicios/derecho-laboral">Derecho Laboral</Link>
+            </MenuItem>
+          </Collapse>
+          <MenuItem disableGutters onClick={toggleMobileMenu}>
+            <Link href="/blog" className="font-bold text-white pl-4">
+              Artículos
+            </Link>
+          </MenuItem>
+          <MenuItem disableGutters onClick={toggleMobileMenu}>
+            <Link href="/contacto" className="font-bold text-white pl-4">
+              Contacto
+            </Link>
+          </MenuItem>
+          <div className="w-full mt-4 pt-8 border-t border-t-secondary flex flex-row justify-center">
+            <a
+              href={whatsappLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="bg-secondary text-white rounded py-3 px-8"
+            >
+              Contáctanos
+            </a>
+          </div>
         </div>
       </Drawer>
     </AppBar>
